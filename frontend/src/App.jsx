ce3f43d317c633e409f0076b68f8dc6943847ba1
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-03-17 20:35:05 +0530

    Routing setup done in frontend via RRDv6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
 export default App;
