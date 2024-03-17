f366d4c34c87a7a77757313fa6033b95e85b3a27
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-03-18 00:07:46 +0530

    Register Page created

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,8 +1,50 @@
+import { useState } from 'react';
 import './Register.css'
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [secretAnswer, setSecretAnswer] = useState("");
   return (
-    <div>Register</div>
+    <div className="register">
+      <form className='register_form'>
+        <h1>REGISTER </h1>
+        <input
+          type="text"
+          placeholder="Enter your Name...."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your Password...."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Enter your Phone no...."
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter your Address...."
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter your Secret answer...."
+          onChange={(e) => setSecretAnswer(e.target.value)}
+        />
+        <button>Register</button>
+      </form>
+    </div>
   )
 }
 
