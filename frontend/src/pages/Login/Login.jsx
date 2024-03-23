332c88a5bcfb7d51cb8d0fbb486180ec3df3c929
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-03-23 11:38:28 +0530

    Login API integrated in the frontend

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    const response = await axios.post(
+      `http://localhost:8080/api/v1/user/login`,
+      { email, password }
+    );
+    if ((!email, !password)) {
+      alert(`Please Fill all the entries`);
+    }
+    if (response.status === 200) {
+      navigate("/");
+      alert(`Login Succesfull`);
+    }
   };
   return (
     <div className="login">
